5dcb85aae8fe36ff79adeaf1c590925631c25670
Taeyoon Lee <taeyoon0137@gmail.com>
2023-09-22 19:55:10 +0900

    init: [type-email] Add type-email library

---
+++ lib/type-email/src/types/Email.type.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2023 Whatssub Co., Ltd. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * ### Email
+ *
+ * Email string literal type
+ */
+export type Email = `${string}@${string}.${string}`;
