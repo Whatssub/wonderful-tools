34a2d9166c90ff16a2e0c5a92a8c0577f2126485
Taeyoon Lee <taeyoon0137@gmail.com>
2023-09-22 19:58:13 +0900

    update: Make root package to publishable

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2023 Whatssub Co., Ltd. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export * from '@wonderful-tools/fp';
+export * from '@wonderful-tools/node';
+export * from '@wonderful-tools/type';
+export * from '@wonderful-tools/util';
