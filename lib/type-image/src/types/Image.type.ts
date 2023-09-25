2df33b2bc4fadc1590d6015a0f56c66bd4d58905
Taeyoon Lee <taeyoon0137@gmail.com>
2023-09-25 10:26:34 +0900

    add: [type] Additional type libraries

---
+++ lib/type-image/src/types/Image.type.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2023 Whatssub Co., Ltd. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * ### Image
+ *
+ * Image literal type
+ *
+ * _Treated as string_
+ */
+export type Image = string;
