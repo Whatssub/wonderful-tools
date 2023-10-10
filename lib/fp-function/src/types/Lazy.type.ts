614bb446d422067d98ea05a31e6a9e8c5f587966
whatisyourname0 <junii@whatssub.co>
2023-10-10 23:45:38 +0900

    feat: [fp-function] initialize fp-function package

---
+++ lib/fp-function/src/types/Lazy.type.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2023 Whatssub Co., Ltd. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * ### Lazy type
+ *
+ * Type of value that is evaluated in lazy manner
+ */
+export interface Lazy<T> {
+  (): T;
+}
